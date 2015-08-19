8fc03fd3c5ae95ef357751529dd8d75834320672
Heikki Linnakangas <hlinnakangas@pivotal.io>
2015-08-19 16:20:22 -0700

    [MPP-25631] Make the spilltodisk regression test result smaller.
    The expected output file was enormous, 17 MB. Instead of outputting the
    enormous query results, write the results to temporary tables, and only
    output some aggregate results.

--- src/test/regress/bugbuster/sql/spilltodisk.sql
+++ src/test/regress/bugbuster/sql/spilltodisk.sql
@@ -12,10 +12,15 @@ set enable_hashjoin=off;
 set enable_nestloop=off;
 set statement_mem=10000;
 
-select t1.*, t2.*
+create temporary table testresult1 as
+select t1.a as t1a, t2.a as t2a
 from (select a from test1 group by a) t1,
      (select a from test2 group by a) t2
 where t1.a = t2.a;
+
+-- check that the result looks sane
+select count(*), sum(t1a), sum(t2a), sum(t1a - t2a) from testresult1;
+
 -- Test Hash Aggregation when the work mem is too small for the hash table
 drop table if exists test;
 create table test (a integer, b integer);
@@ -38,4 +43,7 @@ set enable_nestloop=off;
 set enable_hashjoin=on;
 set statement_mem=10000;
 
-select t1.* from test t1, test t2 where t1.a = t2.a;
+create temporary table testresult2 as
+select t1.a as t1a, t1.b as t1b, t2.a as t2a, t2.b as t2b from test t1, test t2 where t1.a = t2.a;
+-- check that the result looks sane
+select count(*), sum(t1a), sum(t2a), sum(t2a), sum(t2b), sum(t1a * t1b) from testresult2;
