slari-pivotal/gpdb

commit 26228b5cc6ce8d1a0b905ef2f81e10821034d540

[MPP-25631] Remove dead code (service.c/service.h)
This also gets rid of the unused SuppressPanic mechanism. Good riddance, it
seemed exteremely dangerous. Fortunately, it was dead code.

src/backend/utils/misc/faultinjector.c
@@ -27,7 +27,6 @@
 #include "postmaster/checkpoint.h"
 #include "cdb/cdbutil.h"
 #include "postmaster/fts.h"
-#include "postmaster/service.h"
 #include "storage/spin.h"
 #include "storage/shmem.h"
 #include "utils/faultinjector.h"
@@ -773,24 +772,6 @@ FaultInjector_InjectFaultIfSet(
 		case FaultInjectorTypeResume:
 			break;
 			
-		case FaultInjectorTypePanicSuppress:
-		{
-			DECLARE_SAVE_SUPPRESS_PANIC();
-			
-			entryLocal->faultInjectorState = FaultInjectorStateCompleted;
-			
-			FaultInjector_UpdateHashEntry(entryLocal);	
-			
-			SUPPRESS_PANIC();
-			
-			ereport(FATAL, 
-					(errmsg("fault triggered, fault name:'%s' fault type:'%s' ",
-							FaultInjectorIdentifierEnumToString[entryLocal->faultInjectorIdentifier],
-							FaultInjectorTypeEnumToString[entryLocal->faultInjectorType])));	
-			
-			break;
-		}
-
 		case FaultInjectorTypeSegv:
 		{
 			*(int *) 0 = 1234;

src/include/utils/faultinjector.h
@@ -243,8 +243,6 @@ typedef enum FaultInjectorType_e {
 	
 	FaultInjectorTypeStatus,
 	
-	FaultInjectorTypePanicSuppress,
-
 	FaultInjectorTypeSegv,
 	
 	FaultInjectorTypeInterrupt,

src/backend/cdb/cdbfts.c
@@ -28,7 +28,6 @@
 
 #include "executor/spi.h"
 
-#include "postmaster/service.h"
 #include "postmaster/fts.h"
 #include "postmaster/primary_mirror_mode.h"
 #include "utils/faultinjection.h"

src/backend/postmaster/Makefile
@@ -13,7 +13,7 @@ top_builddir = ../../..
 include $(top_builddir)/src/Makefile.global
 override CPPFLAGS := -I$(top_srcdir)/src/backend/gp_libpq_fe $(CPPFLAGS)
 
-OBJS = bgwriter.o autovacuum.o service.o checkpoint.o seqserver.o pgarch.o pgstat.o \
+OBJS = bgwriter.o autovacuum.o checkpoint.o seqserver.o pgarch.o pgstat.o \
 	postmaster.o primary_mirror_mode.o primary_mirror_transition_client.o syslogger.o \
 	fork_process.o perfmon.o backoff.o perfmon_segmentinfo.o \
 	sendalert.o alertseverity.o

src/backend/utils/error/elog.c
@@ -194,8 +194,6 @@ static char formatted_start_time[FORMATTED_TS_LEN];
 		} \
 	} while (0)
 
-bool SuppressPanic = false;   /* GP */
-
 static void cdb_tidy_message(ErrorData *edata);
 static void log_line_prefix(StringInfo buf);
 static void send_message_to_server_log(ErrorData *edata);
@@ -318,11 +316,7 @@ errstart(int elevel, const char *filename, int lineno,
 		 * If we are inside a critical section, all errors become PANIC
 		 * errors.	See miscadmin.h.
 		 */
-         /*
-          * TODO  Chuck asks:  Isn't it dangerous to supress the PANIC?
-          * Can't we do this in a better way?
-          */
-		if (CritSectionCount > 0 && !SuppressPanic)
+		if (CritSectionCount > 0)
 			elevel = PANIC;
 
 		/*
@@ -561,9 +555,6 @@ errfinish(int dummy __attribute__((unused)),...)
 		 * GP: While doing local error try/catch, do not reset all these
 		 * important variables!
 		 */
-		if (!SuppressPanic)
-		{  // GP: Don't indent so we can watch this section...
-
 		/*
 		 * We do some minimal cleanup before longjmp'ing so that handlers can
 		 * execute in a reasonably sane state.
@@ -582,7 +573,6 @@ errfinish(int dummy __attribute__((unused)),...)
 		InterruptHoldoffCount = 0;
 
 		CritSectionCount = 0;	/* should be unnecessary, but... */
-		}// GP: End watching section.
 
 		/*
 		 * Note that we leave CurrentMemoryContext set to ErrorContext. The

src/include/postmaster/service.h
@@ -1,117 +1,0 @@
-/*-------------------------------------------------------------------------
- *
- * server.c
- *	  Shared code module that supports Greenplum system server processes
- *    under the postmaster that communicate with other Postgres processes
- *    with TCP/IP (same GP instance or different GP instance).
- *
- * Copyright (c) 2007-2008, Greenplum inc
- *
- *-------------------------------------------------------------------------
- */
-#ifndef SERVICE_H
-#define SERVICE_H
-
-#ifndef _WIN32
-#include <stdint.h>             /* uint32_t (C99) */
-#else
-typedef unsigned int uint32_t;
-#endif 
-
-typedef struct ServiceCtrl
-{
-
-	void    *serviceConfig;
-
-	int 	listenerFd;
-
-	int		listenerPort;
-	
-}	ServiceCtrl;
-
-
-#define SERVER_EYE_CATCHER_LEN 6
-
-
-/*
- * Kludge to prevent our error handling during communication with master standby by raising and
- * catching ERROR from becoming a PANIC in critical sections... 
- * 
- * The reason for kludge is to avoid postmaster reset on master due to master standby issues. 
- *
- * However PANIC needs to be raised when transaction is in commit state.
- * Justification 
- *		a) to be able to recover GPDB (i.e. avoid commit and abort for the same transaction in the XLOG)
- *		b) to have consistent behavior with other Transaction Management design/code
- *			*) During transaction commit the code is in Critical section, any failure causes PANIC
- *			*) After transaction commit there is a check in AbortTransaction. If transaction has been already 
- *				committed then PANIC is raised
- */
-#define DECLARE_SAVE_SUPPRESS_PANIC()\
-	volatile int SaveSuppressPanic = false; \
-	bool IsCommit = IsCommitInProgress();
-
-#define SUPPRESS_PANIC()\
-{\
-	if (! IsCommit) \
-	{ \
-		SaveSuppressPanic = SuppressPanic;\
-		SuppressPanic = true;\
-	} \
-}
-
-#define RESTORE_PANIC()\
-{\
-	if (! IsCommit) \
-	{ \
-		SuppressPanic = SaveSuppressPanic;\
-	} \
-}
-
-
-typedef struct ServiceConfig
-{
-	char    eyeCatcher[SERVER_EYE_CATCHER_LEN];
-
-	char		*title;
-	char		*psTitle;
-	char		*forkTitle;
-
-	int			requestLen;
-	int			responseLen;
-
-	void		(*ServiceClientTimeout) (struct timeval *clientTimeout);
-	void		(*ServiceRequestShutdown) (SIGNAL_ARGS);
-	void		(*ServiceEarlyInit) (void);
-	void		(*ServicePostgresInit) (void);
-	void		(*ServiceInit) (int listenerPort);
-	bool		(*ServiceShutdownRequested) (void);
-	bool		(*ServiceRequest) (ServiceCtrl *serviceCtrl, int sockfd, uint8 *request);
-	void		(*ServiceShutdown) (void);
-	void		(*ServicePostmasterDied) (void);
-	
-}	ServiceConfig;
-
-typedef struct ServiceClient
-{
-	ServiceConfig    *serviceConfig;
-
-	int 	sockfd;
-	
-}	ServiceClient;
-
-extern void ServiceInit(ServiceConfig *serviceConfig, ServiceCtrl *serviceCtrl);
-extern void ServiceMain(ServiceCtrl *serviceCtrl);
-extern void ServiceGetClientTimeout(ServiceConfig *serviceConfig, struct timeval *timeout); 
-extern bool ServiceClientConnect(ServiceConfig *serviceConfig, int listenerPort, ServiceClient *serverClient, bool complain);
-extern bool ServiceClientSendRequest(ServiceClient *serverClient, void* request, int requestLen);
-extern bool ServiceClientReceiveResponse(ServiceClient *serverClient, void* response, int responseLen, struct timeval *timeout);
-extern bool ServiceClientPollResponse(ServiceClient *serverClient, void* response, int responseLen, bool *pollResponseReceived);
-extern char *ServiceGetLastClientErrorString(void);
-extern bool ServiceProcessRespond(ServiceCtrl *serviceCtrl, int sockfd, uint8 *response, int responseLen);
-
-#ifdef EXEC_BACKEND
-extern pid_t ServiceForkExec(ServiceConfig *serviceConfig);
-#endif
-
-#endif   /* SERVICE_H */

src/include/utils/elog.h
@@ -269,9 +269,6 @@ extern int errSendAlert(bool sendAlert);		/* GPDB: Send alert via e-mail or SNMP
 
 extern int errSuppressOutputToLog(void);   /* GP */
 
-// Generally a good idea!
-extern bool SuppressPanic;   /* GP */
-
 /*----------
  * Old-style error reporting API: to be used in this way:
  *		elog(ERROR, "portal \"%s\" not found", stmt->portalname);

commit 8aa44e270ed60fa3a5a1f276b61dce5582baf868

[MPP-25631] Fix list of fault-injector types.
Broken in commit 26228b5c. I didn't notice that there are three places
that list the fault-injector types, and the all need to be kept in sync
with each other. To avoid this kind of bugs in the future, add comments to
all three places that they need to be kept in sync.

gpMgmt/bin/gppylib/programs/clsInjectFault.py
@@ -294,6 +294,9 @@ class GpInjectFaultProgram:
         # these options are used to build the message for the segments
         addTo = OptionGroup(parser, 'Fault Options: ')
         parser.add_option_group(addTo)
+        # NB: This list needs to be kept in sync with:
+        # - FaultInjectorTypeEnumToString
+        # - FaultInjectorType_e
         addTo.add_option('-y','--type', dest="type", type='string', metavar="<type>",
                          help="fault type: sleep (insert sleep), fault (report fault to postmaster and fts prober), " \
 			      "fatal (inject FATAL error), panic (inject PANIC error), error (inject ERROR), " \
@@ -302,7 +305,6 @@ class GpInjectFaultProgram:
 			      "skip (inject skip i.e. skip checkpoint), " \
 			      "memory_full (all memory is consumed when injected), " \
 			      "reset (remove fault injection), status (report fault injection status), " \
-			      "panic_suppress (inject suppressed PANIC in critical section), " \
 			      "segv (inject a SEGV), " \
 			      "interrupt (inject an Interrupt), " \
 			      "checkpoint_and_panic (inject a panic following checkpoint) ")

src/backend/utils/misc/faultinjector.c
@@ -72,6 +72,11 @@ static int FaultInjector_UpdateHashEntry(
 static bool FaultInjector_RemoveHashEntry(
 								FaultInjectorIdentifier_e identifier);
 
+/*
+ * NB: This list needs to be kept in sync with:
+ * - FaultInjectorType_e
+ * - the help message in clsInjectFault.py
+ */
 const char*
 FaultInjectorTypeEnumToString[] = {
 	_(""), /* not specified */
@@ -88,7 +93,6 @@ FaultInjectorTypeEnumToString[] = {
 	_("memory_full"),
 	_("reset"),
 	_("status"),
-	_("panic_suppress"),
 	_("segv"),
 	_("interrupt"),
 	_("checkpoint_and_panic"),

src/include/utils/faultinjector.h
@@ -212,7 +212,9 @@ typedef enum FaultInjectorIdentifier_e {
 } FaultInjectorIdentifier_e;
 
 /*
- *
+ * NB: This list needs to be kept in sync with:
+ * - FaultInjectorTypeEnumToString
+ * - the help message in clsInjectFault.py
  */
 typedef enum FaultInjectorType_e {
 	FaultInjectorTypeNotSpecified=0,