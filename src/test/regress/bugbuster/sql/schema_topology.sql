f46935128c35729833d806f6109ffd34d9a296a5
Jimmy Yih <jyih@gopivotal.com>
2015-09-24 13:04:52 -0700

    Remove customer name references for OSS.
    Signed-off-by: Nikhil Kak <nkak@pivotal.io>

--- src/test/regress/bugbuster/sql/schema_topology.sql
+++ src/test/regress/bugbuster/sql/schema_topology.sql
@@ -3037,7 +3037,7 @@ CREATE CAST (varchar AS text) WITHOUT FUNCTION AS IMPLICIT;
 
 -- Heap Table 
 
-CREATE TABLE ebay_heap (
+CREATE TABLE be_heap (
     datacenter character varying(32),
     poolname character varying(256),
     machinename character varying(256),
@@ -3057,21 +3057,21 @@ CREATE TABLE ebay_heap (
 
 -- Insert values into the Heap Table
 
-insert into ebay_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,276,'A','2008-11-08 00:01:10.27','EXEC','2250293824','0',39.00,'userlookupreadhost',1,'value_1');
-insert into ebay_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,277,'A','2008-11-08 00:01:10.30','EXEC','3722270207','0',39.00,'userwrite17host',2,'value_2');
-insert into ebay_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,278,'A','2008-11-08 00:01:10.34','EXEC','2098551587','0',40.00,'userwrite17host',3,'value_3');
-insert into ebay_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',4,'value_4');
-insert into ebay_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',5,'value_5');
-insert into ebay_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',generate_series(6,100),repeat('text',100));
+insert into be_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,276,'A','2008-11-08 00:01:10.27','EXEC','2250293824','0',39.00,'userlookupreadhost',1,'value_1');
+insert into be_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,277,'A','2008-11-08 00:01:10.30','EXEC','3722270207','0',39.00,'userwrite17host',2,'value_2');
+insert into be_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,278,'A','2008-11-08 00:01:10.34','EXEC','2098551587','0',40.00,'userwrite17host',3,'value_3');
+insert into be_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',4,'value_4');
+insert into be_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',5,'value_5');
+insert into be_heap values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',generate_series(6,100),repeat('text',100));
 
 
 -- Select from the Heap Table
 
-select count(*) from ebay_heap ;
+select count(*) from be_heap ;
 
 -- AO Table with quicklz compression level 1
 
-CREATE TABLE ebay_quicklz_1 (
+CREATE TABLE be_quicklz_1 (
     datacenter character varying(32),
     poolname character varying(256),
     machinename character varying(256),
@@ -3091,19 +3091,19 @@ CREATE TABLE ebay_quicklz_1 (
 
 -- Insert values into the AO quicklz 1 table
 
-insert into ebay_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,276,'A','2008-11-08 00:01:10.27','EXEC','2250293824','0',39.00,'userlookupreadhost',1,'value_1');
-insert into ebay_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,277,'A','2008-11-08 00:01:10.30','EXEC','3722270207','0',39.00,'userwrite17host',2,'value_2');
-insert into ebay_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,278,'A','2008-11-08 00:01:10.34','EXEC','2098551587','0',40.00,'userwrite17host',3,'value_3');
-insert into ebay_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',4,'value_4');
-insert into ebay_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',generate_series(5,100),repeat('text',100));
+insert into be_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,276,'A','2008-11-08 00:01:10.27','EXEC','2250293824','0',39.00,'userlookupreadhost',1,'value_1');
+insert into be_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,277,'A','2008-11-08 00:01:10.30','EXEC','3722270207','0',39.00,'userwrite17host',2,'value_2');
+insert into be_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,278,'A','2008-11-08 00:01:10.34','EXEC','2098551587','0',40.00,'userwrite17host',3,'value_3');
+insert into be_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',4,'value_4');
+insert into be_quicklz_1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',generate_series(5,100),repeat('text',100));
 
 -- Select from AO quicklz 3 table
 
-select count(*) from ebay_quicklz_1 ;
+select count(*) from be_quicklz_1 ;
 
 -- AO Table with quicklz compression level 1
 
-CREATE TABLE ebay_quicklz1 (
+CREATE TABLE be_quicklz1 (
     datacenter character varying(32),
     poolname character varying(256),
     machinename character varying(256),
@@ -3123,16 +3123,16 @@ CREATE TABLE ebay_quicklz1 (
 
 -- Insert values into the AO quicklz 1 table 
 
-insert into ebay_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,276,'A','2008-11-08 00:01:10.27','EXEC','2250293824','0',39.00,'userlookupreadhost',1,'value_1');
-insert into ebay_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,277,'A','2008-11-08 00:01:10.30','EXEC','3722270207','0',39.00,'userwrite17host',2,'value_2');
-insert into ebay_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,278,'A','2008-11-08 00:01:10.34','EXEC','2098551587','0',40.00,'userwrite17host',3,'value_3');
-insert into ebay_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',4,'value_4');
-insert into ebay_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',5,'value_5');
-insert into ebay_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',generate_series(6,100),repeat('text',100));
+insert into be_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,276,'A','2008-11-08 00:01:10.27','EXEC','2250293824','0',39.00,'userlookupreadhost',1,'value_1');
+insert into be_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,277,'A','2008-11-08 00:01:10.30','EXEC','3722270207','0',39.00,'userwrite17host',2,'value_2');
+insert into be_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,278,'A','2008-11-08 00:01:10.34','EXEC','2098551587','0',40.00,'userwrite17host',3,'value_3');
+insert into be_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,279,'A','2008-11-08 00:01:10.39','EXEC','1143318638','0',39.00,'userwrite17host',4,'value_4');
+insert into be_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',5,'value_5');
+insert into be_quicklz1 values ('den02','v3listingsintl','hr-wlst012','491539361b0000035e5be7d916',173,280,'A','2008-11-08 00:01:10.42','EXEC','1143318638','0',35.00,'userwrite17host',generate_series(6,100),repeat('text',100));
 
 -- Select from AO quicklz 1 table
 
-select count(*) from ebay_quicklz1 ;
+select count(*) from be_quicklz1 ;
 
 -- Adding checkpoint
 checkpoint;
