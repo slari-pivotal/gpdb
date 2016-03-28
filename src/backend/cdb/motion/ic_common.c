7e8f391dfdd8945573d8b621533626813f8f7684
tangpengzhou <ptang@pivotal.io>
2016-03-28 23:47:48 +0000

    Fix tcp socket/port leak when interconnect type is udp or udpifc
    Since gpdb don't allow changing interconnect type after connection started, it's unnecessary to
    allocate a tcp socket and port for hot switching from udp/udpifc to tcp.

--- src/backend/cdb/motion/ic_common.c
+++ src/backend/cdb/motion/ic_common.c
@@ -302,10 +302,11 @@ InitMotionLayerIPC(void)
 	/*activated = false;*/
 	savedSeqServerFd = -1;
 
-	InitMotionTCP(&TCP_listenerFd, &tcp_listener);
-	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		InitMotionTCP(&TCP_listenerFd, &tcp_listener);
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
 		InitMotionUDPIFC(&UDP_listenerFd, &udp_listener);
-	else if (Gp_interconnect_type == INTERCONNECT_TYPE_UDP)
+	else (Gp_interconnect_type == INTERCONNECT_TYPE_UDP)
 		InitMotionUDP(&UDP_listenerFd, &udp_listener);
 
 	Gp_listener_port = (udp_listener<<16) | tcp_listener;
