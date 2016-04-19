slari-pivotal/gpdb

commit b8d65a39d23213b60360824ff1b83ac28145b26f

Add fully_qualified_domain_name option for gpload. (#12)
If it is true, gpload will try to get its fully qualified domain name (fqdn), for example "myhost.mydomain.mytld".
If not set, the hostname would be "myhost".
This is useful when gpload doens't locate in same LAN with GPDB.
Build and unittest pass.

gpMgmt/bin/gpload.py
@@ -44,6 +44,7 @@ import hashlib
 import datetime,getpass,os,signal,socket,subprocess,threading,time,traceback,re
 import platform
 import uuid
+import socket
 
 thePlatform = platform.system()
 if thePlatform in ['Windows', 'Microsoft']:
@@ -102,6 +103,7 @@ valid_tokens = {
     "error_table": {'parse_children': True, 'parent': "input"},
     "log_errors": {'parse_children': False, 'parent': "input"},
     "header": {'parse_children': True, 'parent': "input"},
+    "fully_qualified_domain_name": {'parse_children': False, 'parent': 'input'},
     "output": {'parse_children': True, 'parent': "gpload"},
     "table": {'parse_children': True, 'parent': "output"}, 
     "mode": {'parse_children': True, 'parent': "output"},
@@ -1619,15 +1621,13 @@ class gpload:
 
             # do default host, the current one
             if not local_hostname:
-                try:
-                    pipe = subprocess.Popen("hostname",
-                                            stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE)
-                    result  = pipe.communicate();
-                except OSError, e:
-                    self.log(self.ERROR, "command failed: " + str(e))
-                
-                local_hostname = [result[0].strip()]
+                # if fully_qualified_domain_name is defined and set to true we want to
+                # resolve the fqdn rather than just grabbing the hostname.
+                fqdn = self.getconfig('gpload:input:fully_qualified_domain_name', bool, False)
+                if fqdn:
+                    local_hostname = socket.getfqdn()
+                else:
+                    local_hostname = socket.gethostname()
 
             # build gpfdist parameters
             popenList = ['gpfdist']

gpMgmt/bin/gppylib/test/unit/allconfig.yml
@@ -1,0 +1,33 @@
+VERSION: 1.0.0.1
+DATABASE: odbcdb 
+USER: gpadmin
+HOST: mdw
+PORT: 16789
+
+GPLOAD:
+  INPUT:
+    - SOURCE:
+        LOCAL_HOSTNAME:
+          - mdw
+        PORT: 1981
+        FILE:
+          - /data/home/gpadmin/yydzero/work/sample.csv
+    - COLUMNS:
+        - id: int
+        - msg: text
+    - FORMAT: csv
+    - QUOTE: '"'
+    - FULLY_QUALIFIED_DOMAIN_NAME: true
+  OUTPUT:
+    - TABLE: test
+    - MODE: merge
+    - MATCH_COLUMNS:
+        - id
+    - UPDATE_COLUMNS:
+        - msg
+    - MAPPING:
+        id: id
+        msg: msg
+
+  PRELOAD:
+    - REUSE_TABLES: true

gpMgmt/bin/gppylib/test/unit/test_unit_gpload.py
@@ -97,6 +97,14 @@ class GpLoadTestCase(unittest.TestCase):
                               False)
 
 
+    def test_case_configvalue(self):
+        gploader = gpload(['-f', os.path.join(os.path.dirname(__file__), 'allconfig.yml')])
+        self.assertEqual(u'test', gploader.getconfig('gpload:output:table'))
+        self.assertEqual(1981, gploader.getconfig('gpload:input:source:port', int))
+        self.assertEqual(True, gploader.getconfig('gpload:preload:reuse_tables', bool))
+        self.assertEqual(False, gploader.getconfig('gpload:input:log_errors', bool, False))
+        self.assertEqual(True, gploader.getconfig('gpload:input:fully_qualified_domain_name', bool))
+
 #------------------------------- Mainline --------------------------------
 if __name__ == '__main__':
     unittest.main()

commit 23616f7cfbc164545e350758142850fc1b3e71bb

Quick fix for fqdn return result.
"local_hostname" should be a list instead of a single hostname string.
This fix the regression brought by commit https://github.com/greenplum-db/gpdb4/commit/b8d65a39d23213b60360824ff1b83ac28145b26f

gpMgmt/bin/gpload.py
@@ -1626,9 +1626,9 @@ class gpload:
                 # resolve the fqdn rather than just grabbing the hostname.
                 fqdn = self.getconfig('gpload:input:fully_qualified_domain_name', bool, False)
                 if fqdn:
-                    local_hostname = socket.getfqdn()
+                    local_hostname = [socket.getfqdn()]
                 else:
-                    local_hostname = socket.gethostname()
+                    local_hostname = [socket.gethostname()]
 
             # build gpfdist parameters
             popenList = ['gpfdist']