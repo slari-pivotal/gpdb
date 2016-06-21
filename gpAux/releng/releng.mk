b1710c6ebb146cdf668e42d92c84cc23251cd564
Venkatesh (Venky) Raghavan <vraghavan@pivotal.io>
2016-06-21 16:10:15 -0700

    Bumping optimizer version number

--- gpAux/releng/releng.mk
+++ gpAux/releng/releng.mk
@@ -171,7 +171,7 @@ sync_tools: opt_write_test /opt/releng/apache-ant
 	@echo "Resolve finished";
 
 	wget -O - https://github.com/greenplum-db/gpos/releases/download/v1.137/bin_gpos_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
-	wget -O - https://github.com/greenplum-db/gporca/releases/download/v1.637/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
+	wget -O - https://github.com/greenplum-db/gporca/releases/download/v1.640/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
 
 clean_tools: opt_write_test
 	@cd releng/make/dependencies; \
