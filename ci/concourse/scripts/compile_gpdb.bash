76f8330cb6a80859a1c608693b5c9428e4ce8393
Ashwin Agrawal <aagrawal@pivotal.io>
2016-06-21 16:14:46 -0700

    Add executable permissions to all files in install_directory [#121687333]
    Signed-off-by: Ed Espino <edespino@pivotal.io>

--- ci/concourse/scripts/compile_gpdb.bash
+++ ci/concourse/scripts/compile_gpdb.bash
@@ -31,17 +31,12 @@ function unittest_check_gpdb() {
   popd
 }
 
-function set_py_perms() {
-  pushd /usr/local/greenplum-db-devel
-    find . -name "*.py" -exec chmod 755 \{\} \;
-  popd
-}
-
 function export_gpdb() {
   TARBALL=$(pwd)/bin_gpdb/bin_gpdb.tar.gz
   pushd /usr/local/greenplum-db-devel
     source greenplum_path.sh
     python -m compileall -x test .
+    chmod -R 755 .
     tar -czf "${TARBALL}" ./*
   popd
 }
@@ -51,7 +46,6 @@ function _main() {
   make_sync_tools
   build_gpdb
   unittest_check_gpdb
-  set_py_perms
   export_gpdb
 }
 
