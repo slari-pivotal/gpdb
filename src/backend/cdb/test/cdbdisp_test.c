232e0698b09bf94522f34a833a842e65f3e966af
Foyzur Rahman <foyzur@gmail.com>
2016-06-27 17:16:10 -0700

    Restoring 4.3 cdbdisp_test.c because of lack of refactoring as present on master, sans the assert checking as Heikki removed assert checking in 3ff3260.

--- src/backend/cdb/test/cdbdisp_test.c
+++ src/backend/cdb/test/cdbdisp_test.c
@@ -3,33 +3,34 @@
 #include <setjmp.h>
 #include "cmockery.h"
 
-#include "../cdbdisp_query.c"
+#include "../cdbdisp.c"
 
-/*
+/* 
  * Mocked object initializations required for dispatchPlan.
  */
 void
 _init_cdbdisp_dispatchPlan(QueryDesc *queryDesc)
 {
 	queryDesc->estate = (struct EState *)palloc0(sizeof(struct EState));
-	queryDesc->estate->es_sliceTable =
-		(struct SliceTable *) palloc0(sizeof(struct SliceTable));
+	queryDesc->estate->es_sliceTable = (struct SliceTable *)
+                                            palloc0(sizeof(struct SliceTable));
 	queryDesc->operation = CMD_NOTHING;
-	queryDesc->plannedstmt = (PlannedStmt *)palloc0(sizeof(PlannedStmt));
+	queryDesc->plannedstmt = (PlannedStmt *)palloc0(sizeof(PlannedStmt));   
 
 	will_be_called(clear_relsize_cache);
+
 	expect_any(RootSliceIndex, estate);
-	will_return(RootSliceIndex,0);
+	will_return(RootSliceIndex,0);  
 }
 
-/*
- * Tests that cdbdisp_dispatchPlan handles a plan size overflow
+/* 
+ * Tests that cdbdisp_dispatchPlan handles a plan size overflow 
  * when splan_len_uncompressed * num_slices.
  */
 void
 test__cdbdisp_dispatchPlan__Overflow_plan_size_in_kb(void **state)
 {
-	bool success = false;
+	bool		success = false;
 
 	struct CdbDispatcherState *ds = (struct CdbDispatcherState *)
 		palloc0(sizeof(struct CdbDispatcherState));
@@ -39,24 +40,19 @@ test__cdbdisp_dispatchPlan__Overflow_plan_size_in_kb(void **state)
 
 	_init_cdbdisp_dispatchPlan(queryDesc);
 
-	/*
-	 * Set max plan to a value that will require handling INT32
-	 * overflow of the current plan size
-	 */
+	/* Set max plan to a value that will require handling INT32 
+	 * overflow of the current plan size */
 	gp_max_plan_size = INT_MAX;
 
 	queryDesc->plannedstmt->planTree = (struct Plan *)palloc0(sizeof(struct Plan));
 
-	/*
-	 * Set num_slices and uncompressed_size to be INT_MAX-1 to force overflow
-	 */
+	/* Set num_slices and uncompressed_size to be INT_MAX-1 to force overflow */
 	queryDesc->plannedstmt->planTree->nMotionNodes = INT_MAX-1;
 
-	will_assign_value(serializeNode, uncompressed_size_out, INT_MAX-1);
 	expect_any(serializeNode, node);
 	expect_any(serializeNode, size);
 	expect_any(serializeNode, uncompressed_size_out);
-
+	will_assign_value(serializeNode, uncompressed_size_out, INT_MAX-1);
 	will_return(serializeNode, NULL);
 
 	PG_TRY();
@@ -65,10 +61,9 @@ test__cdbdisp_dispatchPlan__Overflow_plan_size_in_kb(void **state)
 	}
 	PG_CATCH();
 	{
-		/*
-		 * Verify that we get the correct error (limit exceeded)
-		 * CopyErrorData() requires us to get out of ErrorContext
-		 */
+		/* Verify that we get the correct error (limit exceeded) */
+
+		/* CopyErrorData() requires us to get out of ErrorContext */
 		CurrentMemoryContext = TopMemoryContext;
 
 		ErrorData *edata = CopyErrorData();
@@ -89,14 +84,13 @@ test__cdbdisp_dispatchPlan__Overflow_plan_size_in_kb(void **state)
 
 	assert_true(success);
 }
-
-int
+	
+int		
 main(int argc, char* argv[])
 {
 	cmockery_parse_arguments(argc, argv);
 
-	const UnitTest tests[] =
-	{
+	const UnitTest tests[] = {
 		unit_test(test__cdbdisp_dispatchPlan__Overflow_plan_size_in_kb)
 	};
 
