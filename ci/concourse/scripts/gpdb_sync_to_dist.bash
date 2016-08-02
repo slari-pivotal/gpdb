cfd0e4f75d197aedc065579d5788fbdd93e4faa2
Andrew Edstrom <aedstrom@pivotal.io>
2016-08-02 14:44:57 -0700

    Change gpdb_installer to artifact_to_upload [#127285279]
    Also remove repeated variables from sync to dist job in the pipeline
    
    The source-to-dist upload is os-independent
    - This fixes some folder-os/filename-os mismatches
    
    - SLES and Centos jobs both need to pass for sync to be triggered
    
    Signed-off-by: Jacob Frank <jfrank@pivotal.io>
    Signed-off-by: Andrew Edstrom <aedstrom@pivotal.io>

--- ci/concourse/scripts/gpdb_sync_to_dist.bash
+++ ci/concourse/scripts/gpdb_sync_to_dist.bash
@@ -2,16 +2,20 @@
 
 set -euo pipefail
 
-function echo_expected_env_variables() {
+REMOTE_USER="build"
+REMOTE_HOST="artifacts.ci.eng.pivotal.io"
+REMOTE_DIRECTORY="/data/dist/GPDB/127285279_pipeline_refactor_builds"
+
+function echo_paths() {
   echo "Target remote directory: $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRECTORY"
   echo "Local file to upload: $FILE_TO_UPLOAD"
 }
 
 function validate_remote_dir() {
-  #To prevent accidental creation of weird directories or arbitrary command
-  #execution by the user, we validate the remote directory.
-  #For example, paths like "/opt" and "/data/dist/GPDB/../../../opt" are not allowed.
-  #grep will fail if the regex does not match and this script will stop due to the set -euo.
+  # To prevent accidental creation of weird directories or arbitrary command
+  # execution by the user, we validate the remote directory.
+  # For example, paths like "/opt" and "/data/dist/GPDB/../../../opt" are not allowed.
+  # grep will fail if the regex does not match and this script will stop due to the set -euo.
   echo -e "Validating that $REMOTE_DIRECTORY is a valid path (must be a" \
           "subdirectory of /data/dist/GPDB)...\c"
   echo "$REMOTE_DIRECTORY" | grep '^/data/dist/GPDB/[/a-zA-Z0-9\_\-]\+$' > /dev/null
@@ -21,20 +25,20 @@ function validate_remote_dir() {
 function scp_zip_src() {
   local remote_path
   local ssh_key_file
-  remote_path="$REMOTE_DIRECTORY/`basename $FILE_TO_UPLOAD`"
+  remote_path="$REMOTE_DIRECTORY/$(basename "$FILE_TO_UPLOAD")"
   ssh_key_file="$(mktemp -t ssh_key)"
   echo "$SSH_KEY" > "$ssh_key_file"
 
-  #We do not use mkdir -p, to prevent the user from accidentally creating random
-  #directories on our server. Also, we use StrictHostKeyChecking=no and
-  #LogLevel=error to avoid printing warning messages such as "permanently added
-  #to the list of known hosts"
+  # We do not use mkdir -p in order to prevent the user from accidentally creating
+  # random directories on our server. Also, we use StrictHostKeyChecking=no and
+  # LogLevel=error to avoid printing warning messages such as "permanently added
+  # to the list of known hosts"
   ssh -i "$ssh_key_file" -o UserKnownHostsFile=/dev/null \
       -o LogLevel=error -o StrictHostKeyChecking=no \
       "$REMOTE_USER@$REMOTE_HOST" "if [ ! -d $REMOTE_DIRECTORY ]; then mkdir $REMOTE_DIRECTORY; fi" > /dev/null
 
-  #scp is not an atomic operation, so we use a scp/ssh mv to avoid broken files
-  #being left sitting on the server
+  # scp is not an atomic operation, so we use a scp/ssh mv to avoid broken files
+  # being left sitting on the server
   scp -i "$ssh_key_file" -o UserKnownHostsFile=/dev/null \
       -o LogLevel=error -o StrictHostKeyChecking=no \
       "$FILE_TO_UPLOAD" "$REMOTE_USER@$REMOTE_HOST:$remote_path.new" > /dev/null
@@ -44,16 +48,18 @@ function scp_zip_src() {
 }
 
 function echo_completion() {
-  # remove '/data' off of the remote directory as not present in the URL
+  # Remove '/data' off of the remote directory as not present in the URL
   local remote_dir_url=${REMOTE_DIRECTORY:5}
   # artifacts and artifacts-cache are mirrored, but we show the user
   # artifacts-cache as it is faster to access on the network
-  local remote_host_cache=`echo $REMOTE_HOST | sed 's/^artifacts[.]/artifacts-cache./'`
-  echo "Uploaded file: http://$remote_host_cache$remote_dir_url/`basename $FILE_TO_UPLOAD`"
+  local remote_host_cache
+  remote_host_cache=$(echo $REMOTE_HOST | sed 's/^artifacts[.]/artifacts-cache./')
+
+  echo "Uploaded file: http://${remote_host_cache}${remote_dir_url}/$(basename "$FILE_TO_UPLOAD")"
 }
 
 function _main() {
-  echo_expected_env_variables
+  echo_paths
 
   validate_remote_dir
 
