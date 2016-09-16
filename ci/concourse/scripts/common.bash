63ce8acd50bb30654040609c4d53aefd8e879e8d
C.J. Jameson <cjameson@pivotal.io>
2016-09-15 17:09:22 -0700

    Refactor compile steps into common.bash [#123168181]
    Other CI processes, such as Coverity scanning, can take advantage of a
    shared env setup and `make sync_tools` with the standard compile task

--- ci/concourse/scripts/common.bash
+++ ci/concourse/scripts/common.bash
@@ -9,6 +9,47 @@ function set_env() {
     export TIMEFORMAT=$'\e[4;33mIt took %R seconds to complete this step\e[0m';
 }
 
+function prep_env_for_centos() {
+  case "$TARGET_OS_VERSION" in
+    5)
+      BLDARCH=rhel5_x86_64
+      export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.39.x86_64
+      source /opt/gcc_env.sh
+      ;;
+
+    6)
+      BLDARCH=rhel6_x86_64
+      export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.39.x86_64
+      ;;
+
+    7)
+      BLDARCH=rhel7_x86_64
+      alternatives --set java /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.111-2.6.7.2.el7_2.x86_64/jre/bin/java
+      export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.111-2.6.7.2.el7_2.x86_64
+      ln -sf /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
+      source /opt/gcc_env.sh
+      ;;
+
+    *)
+    echo "TARGET_OS_VERSION not set or recognized for Centos/RHEL"
+    exit 1
+    ;;
+  esac
+
+  ln -sf "/$(pwd)/gpdb_src/gpAux/ext/${BLDARCH}/python-2.6.2" /opt/python-2.6.2
+  export PATH=${JAVA_HOME}/bin:${PATH}
+}
+
+function generate_build_number() {
+  pushd gpdb_src
+    #Only if its git repro, add commit SHA as build number
+    # BUILD_NUMBER file is used by getversion file in GPDB to append to version
+    if [ -d .git ] ; then
+      echo "commit: $(git rev-parse HEAD)" > BUILD_NUMBER
+    fi
+  popd
+}
+
 ## ----------------------------------------------------------------------
 ## Test functions
 ## ----------------------------------------------------------------------
@@ -20,6 +61,13 @@ function install_gpdb() {
 
 function install_sync_tools() {
     tar -xzf sync_tools_gpdb/sync_tools_gpdb.tar.gz -C gpdb_src/gpAux
+}
+
+function make_sync_tools() {
+  pushd gpdb_src/gpAux
+    make IVYREPO_HOST="$IVYREPO_HOST" IVYREPO_REALM="$IVYREPO_REALM" IVYREPO_USER="$IVYREPO_USER" IVYREPO_PASSWD="$IVYREPO_PASSWD" sync_tools
+    tar -czf "$GPDB_ARTIFACTS_DIR/sync_tools_gpdb.tar.gz" ext
+  popd
 }
 
 function configure() {
--- ci/concourse/scripts/compile_gpdb.bash
+++ ci/concourse/scripts/compile_gpdb.bash
@@ -8,61 +8,12 @@ GPDB_ARTIFACTS_DIR=$(pwd)/$OUTPUT_ARTIFACT_DIR
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${CWDIR}/common.bash"
 
-function prep_env_for_centos() {
-  case "$TARGET_OS_VERSION" in
-    5)
-      BLDARCH=rhel5_x86_64
-      export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.39.x86_64
-      source /opt/gcc_env.sh
-      ;;
-
-    6)
-      BLDARCH=rhel6_x86_64
-      export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.39.x86_64
-      ;;
-
-    7)
-      BLDARCH=rhel7_x86_64
-      alternatives --set java /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.111-2.6.7.2.el7_2.x86_64/jre/bin/java
-      export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.111-2.6.7.2.el7_2.x86_64
-      ln -sf /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
-      source /opt/gcc_env.sh
-      ;;
-
-    *)
-    echo "TARGET_OS_VERSION not set or recognized for Centos/RHEL"
-    exit 1
-    ;;
-  esac
-
-  ln -sf /$(pwd)/gpdb_src/gpAux/ext/${BLDARCH}/python-2.6.2 /opt/python-2.6.2
-  export PATH=${JAVA_HOME}/bin:${PATH}
-}
-
 function prep_env_for_sles() {
   ln -sf "$(pwd)/gpdb_src/gpAux/ext/sles11_x86_64/python-2.6.2" /opt
   export JAVA_HOME=/usr/lib64/jvm/java-1.6.0-openjdk-1.6.0
   export PATH=${JAVA_HOME}/bin:${PATH}
   source /opt/gcc_env.sh
 }
-
-function generate_build_number() {
-  pushd gpdb_src
-    #Only if its git repro, add commit SHA as build number
-    # BUILD_NUMBER file is used by getversion file in GPDB to append to version
-    if [ -d .git ] ; then
-      echo "commit:`git rev-parse HEAD`" > BUILD_NUMBER
-    fi
-  popd
-}
-
-function make_sync_tools() {
-  pushd gpdb_src/gpAux
-    make IVYREPO_HOST=${IVYREPO_HOST} IVYREPO_REALM="${IVYREPO_REALM}" IVYREPO_USER=${IVYREPO_USER} IVYREPO_PASSWD=${IVYREPO_PASSWD} sync_tools
-    tar -czf "$GPDB_ARTIFACTS_DIR/sync_tools_gpdb.tar.gz" ext
-  popd
-}
-
 
 function build_gpdb() {
   pushd gpdb_src/gpAux
