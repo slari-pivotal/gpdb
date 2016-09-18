ce88748dd5c0a6229b4a4ebca9024fe03dd91636
Adam Lee <ali@pivotal.io>
2016-09-18 16:59:49 +0800

    Update s3ext concourse pipeline files
    To reuse pivotaldata's docker images and secrets, also it runs tests
    with latest GPDB binary now.
    
    Signed-off-by: Haozhou Wang <hawang@pivotal.io>

---
+++ ci/concourse/scripts/unit_tests_s3.bash
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+set -euxo pipefail
+
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${CWDIR}/common.bash"
+
+function gen_env(){
+	cat > ~/run_unit_tests.sh <<-EOF
+	cd "\${1}/gpdb_src/gpAux/extensions/gps3ext"
+	set -euxo pipefail
+	make test
+	EOF
+
+	chmod a+x ~/run_unit_tests.sh
+}
+
+function run_unit_tests() {
+	bash -e ~/run_unit_tests.sh $(pwd)
+}
+
+function _main() {
+	time gen_env
+
+	time run_unit_tests
+}
+
+_main "$@"
