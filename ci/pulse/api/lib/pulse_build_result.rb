b151dd07e6b892ec78ddfb47a7d51e251eb9f10d
Michael Schubert <pair+mschubert@pivotal.io>
2016-10-20 15:14:57 -0400

    Rewrite monitor and trigger pulse to be idiomatic ruby. Forgive network hiccups. Vary polling. Print build log artifacts.
    [#132835383,#132840667,#132629801,#126747379]

---
+++ ci/pulse/api/lib/pulse_build_result.rb
@@ -0,0 +1,60 @@
+require "net/http"
+require "uri"
+
+class PulseBuildResult
+
+  def initialize(pulse_options, build_result, build_messages, build_artifacts)
+    @pulse_options = pulse_options
+    @build_result = build_result
+    @build_messages = build_messages
+    @build_artifacts = build_artifacts
+  end
+
+  def build_successful?
+    @build_result['succeeded'] == "succeeded"
+  end
+
+  def print_build_stats
+    puts "Build ID fetched from pulse: #{@build_result['id']}"
+
+    puts "Build " + (build_successful? ? "succeeded" : "failed")
+    puts "Revision: #{@build_result['revision']}"
+    puts "   Owner: #{@build_result['owner']}"
+    puts "  Errors: #{@build_result['errorCount']}"
+    puts "Warnings: #{@build_result['warningCount']}"
+
+    seconds = (@build_result['endTimeMillis'].to_i - @build_result['startTimeMillis'].to_i) / 1000
+    puts "Duration: " + (@build_result['completed'] ? Time.at(seconds).utc.strftime("%H:%M:%S") : "N/A")
+
+    puts "=== Messages ==="
+    @build_messages.each do |message|
+      puts "  #{message}"
+    end
+  end
+
+  def print_build_artifacts
+    puts "\n=== Artifacts ===\n"
+    @build_artifacts.each do |artifact|
+      artifact_name = artifact["name"]
+      next unless artifact_name =~ /\.log/
+
+      # convert "Test Results (results.log) into 'results.log'"
+      artifact_name.gsub!(/.*\((.*)\)/) { |match| "#{$1}" }
+      uri_string = [@pulse_options.url, artifact["permalink"], URI.escape(artifact_name)].join("")
+      puts "\n= #{artifact_name} @ #{uri_string} =\n"
+      response = fetch_uri(uri_string)
+      puts response.body
+    end
+  end
+
+  def fetch_uri(uri_string, limit = 10)
+    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+    response = Net::HTTP.get_response(URI.parse(uri_string))
+    case response
+    when Net::HTTPSuccess     then response
+    when Net::HTTPRedirection then fetch_uri(response['location'], limit - 1)
+    else
+      response.error!
+    end
+  end
+end
--- ci/pulse/api/lib/pulse_proxy.rb
+++ ci/pulse/api/lib/pulse_proxy.rb
@@ -9,33 +9,41 @@ class PulseProxy
     @password = password
   end
 
+  def refresh
+    @token = nil
+  end
+
   def request_triggerBuild(project_name, trigger_options)
     #triggerBuild returns an array of request IDs (strings),
     #but only if we pass trigger_options
-    proxy.triggerBuild(token, project_name, trigger_options)
+    retry_call { proxy.triggerBuild(token, project_name, trigger_options) }
   end
 
   def request_waitForBuildRequestToBeActivated(request_id, timeoutMillis)
-    proxy.waitForBuildRequestToBeActivated(token, request_id, timeoutMillis)
+    retry_call { proxy.waitForBuildRequestToBeActivated(token, request_id, timeoutMillis) }
   end
 
   def request_getBuildRequestStatus(request_id)
-    proxy.getBuildRequestStatus(token, request_id)
+    retry_call { proxy.getBuildRequestStatus(token, request_id) }
   end
 
   def request_getBuild(project_name, build_id)
-    proxy.getBuild(token, project_name, build_id)
+    retry_call { proxy.getBuild(token, project_name, build_id) }
   end
 
   def request_getMessagesInBuild(project_name, build_id)
-    proxy.getMessagesInBuild(token, project_name, build_id)
+    retry_call { proxy.getMessagesInBuild(token, project_name, build_id) }
+  end
+
+  def request_getArtifactsInBuild(project_name, build_id)
+    retry_call { proxy.getArtifactsInBuild(token, project_name, build_id) }
   end
 
   # The token will be retrieved one time for this Pulse API connection and
   # reused throughout API calls.
   # Note that the token is only valid for 30 minutes.
   def token
-    @token ||= proxy.login(@username, @password)
+    @token ||= retry_call { proxy.login(@username, @password) }
   end
 
   def proxy
@@ -44,5 +52,18 @@ class PulseProxy
 
   def server
     @server ||= XMLRPC::Client.new2("#{@url}/xmlrpc")
+  end
+
+  def retry_call(&block)
+    attempts = 0
+    begin
+      yield
+    rescue SocketError => error
+      raise error if attempts > 3
+      puts "Got a socket error #{error} - retry ##{attempts}"
+      sleep 10
+      attempts += 1
+      retry
+    end
   end
 end
