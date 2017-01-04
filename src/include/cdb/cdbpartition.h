9eb00c5fcc41b78eb1ecd06d3076e74c48ad4764
foyzur <foyzur@gmail.com>
2017-01-03 16:14:08 -0800

    Removing redundant partitions during a join partition elimination (#1462)
    * Undo selected partitions before reselecting new partitions to avoid unnecessary leftover partitions from previous selections.
    * Adding ICG qp_dpe test to verify that the partitions are reset for each outer tuple.

--- src/include/cdb/cdbpartition.h
+++ src/include/cdb/cdbpartition.h
@@ -266,6 +266,7 @@ extern Node *makeBoolConst(bool value, bool isnull);
 extern LogicalIndexInfo *logicalIndexInfoForIndexOid(Oid rootOid, Oid indexOid);
 
 extern void InsertPidIntoDynamicTableScanInfo(int32 index, Oid partOid, int32 selectorId);
+extern void RemovePartSelectorForPartOid(int32 index, Oid partOid, int32 selectorId);
 
 extern char *
 DebugPartitionOid(Datum *elements, int n);
