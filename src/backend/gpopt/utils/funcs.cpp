9ef20757aa551a1a7f1e8bf8bac5bf07bd7b87a8
Haisheng Yuan <hyuan@pivotal.io>
2017-01-06 13:26:48 -0600

    Revert "Merge gpos into gporca"
    This reverts commit 8e5d678da42c7a0318077c5b4b1abe6bfff8d599.

--- src/backend/gpopt/utils/funcs.cpp
+++ src/backend/gpopt/utils/funcs.cpp
@@ -26,6 +26,7 @@
 #include "gpos/io/CFileWriter.h"
 #include "gpopt/gpdbwrappers.h"
 
+#include "gpos/version.h"
 #include "gpopt/version.h"
 #include "xercesc/util/XercesVersion.hpp"
 
@@ -1046,7 +1047,7 @@ DumpPlanToFile(PG_FUNCTION_ARGS)
 //		LibraryVersion
 //
 //	@doc:
-//		Returns the optimizer and xerces library versions as a message
+//		Returns the optimizer and gpos library versions as a message
 //
 //---------------------------------------------------------------------------
 extern "C" {
@@ -1056,6 +1057,7 @@ LibraryVersion()
 	StringInfoData str;
 	initStringInfo(&str);
 	appendStringInfo(&str, "GPOPT version: %d.%d", GPORCA_VERSION_MAJOR, GPORCA_VERSION_MINOR);
+	appendStringInfo(&str, ", GPOS version: %d.%d", GPOS_VERSION_MAJOR, GPOS_VERSION_MINOR);
 	appendStringInfo(&str, ", Xerces version: %s", XERCES_FULLVERSIONDOT);
 	text *result = stringToText(str.data);
 
