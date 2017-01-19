7a8c942260132273d5c443fcb395121804b07ebf
Tom Meyer <tmeyer@pivotal.io>
2017-01-19 14:05:43 -0800

    Put variables that may contain shell metacharacters in quotes

--- gpAux/releng/releng.mk
+++ gpAux/releng/releng.mk
@@ -173,8 +173,8 @@ sync_tools: opt_write_test /opt/releng/apache-ant
 	@cd releng/make/dependencies; \
 	 (umask 002; ANT_OPTS="-Djavax.net.ssl.trustStore=$(BLD_TOP)/releng/make/dependencies/cacerts" \
 	/opt/releng/apache-ant/bin/ant -DBLD_ARCH=$(BLD_ARCH) \
-	-Divyrepo.host=$(IVYREPO_HOST) -Divyrepo.realm="$(IVYREPO_REALM)" \
-	-Divyrepo.user=$(IVYREPO_USER) -Divyrepo.passwd=$(IVYREPO_PASSWD) resolve);
+	-Divyrepo.host="$(IVYREPO_HOST)" -Divyrepo.realm="$(IVYREPO_REALM)" \
+	-Divyrepo.user="$(IVYREPO_USER)" -Divyrepo.passwd="$(IVYREPO_PASSWD)" resolve);
 	@echo "Resolve finished";
 
 	LD_LIBRARY_PATH="" wget -O - https://github.com/greenplum-db/gporca/releases/download/v2.1/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
