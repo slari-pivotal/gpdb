cloudberrydb/cloudberrydb

Ensure that a StreamNode is always owned by only one StreamBitmap
In this commit, we change the ownership of a StreamNode, so that when a
StreamNode A is added as an input to a StreamNode B, the bitmap that was
pointing to A abandons the ownership and the pointer is set to NULL. In the
above example, when GPDB frees BitmapOR, it will not attempt to free OR's
StreamNode.

In addition, if a StreamNode is an OpStream, then opstream_free function is
used to free the memory used by the StreamNode, which actually uses pfree. On
the other hand, if a StreamNode is an IndexStream, then stream_free function is
invoked, which does not pfree the StreamNode. For consistency, we add function
indexstream_free to always pfree a StreamNode of IndexStream type.

Signed-off-by: Shreedhar Hardikar <shardikar@pivotal.io>

src/test/regress/sql/bitmapscan.sql
@@ -281,3 +281,22 @@ INSERT INTO card_heap_table_w_bitmap (id, v, highCardinalityHighDomain)
 SELECT COUNT(DISTINCT(highCardinalityHighDomain)) AS distinct_hchd FROM card_heap_table_w_bitmap ORDER BY distinct_hchd;
 
 drop table if exists bm_test;
+
+-- Test if StreamNodes and StreamBitmaps are freed correctly.
+create table foo (c int, d int) distributed by (c);
+create index ie_foo on foo using bitmap(d);
+insert into foo values (1, 1);
+insert into foo values (2, 2);
+
+-- Next queries will create additional StreamNodes. In particular,
+-- d in (1, 2) will be transformed into an OR with two BMS_INDEX input streams.
+select * from foo where d in (1, 2) and (d = 1 or d = 2);
+select * from foo where (d = 1 or d = 2) and d in (1, 2);
+
+-- This query will eliminate StreamNodes since there is no tuple where d =3.
+select * from foo where d = 3 and (d = 1 or d = 2);
+
+-- If a segment contains tuples with d in (1, 2), then it will create the whole StreamNode tree, 
+-- otherwise segments will eliminate nodes.
+select * from foo where d = 2 and (d = 1 or d = 2);
+

slari-pivotal/gpdb

src/test/regress/sql/bitmapscan.sql
@@ -277,3 +277,24 @@ INSERT INTO card_heap_table_w_bitmap (id, v, highCardinalityHighDomain)
  SELECT i, CAST(i AS VARCHAR), i % 50000 FROM generate_series(1000001, 1050000) i;
 
 SELECT COUNT(DISTINCT(highCardinalityHighDomain)) AS distinct_hchd FROM card_heap_table_w_bitmap ORDER BY distinct_hchd;
+
+drop table if exists bm_test;
+
+-- Test if StreamNodes and StreamBitmaps are freed correctly.
+create table bm_table_foo (c int, d int) distributed by (c);
+create index ie_bm_table_foo on bm_table_foo using bitmap(d);
+insert into bm_table_foo values (1, 1);
+insert into bm_table_foo values (2, 2);
+
+-- Next queries will create additional StreamNodes. In particular,
+-- d in (1, 2) will be transformed into an OR with two BMS_INDEX input streams.
+select * from bm_table_foo where d in (1, 2) and (d = 1 or d = 2);
+select * from bm_table_foo where (d = 1 or d = 2) and d in (1, 2);
+
+-- This query will eliminate StreamNodes since there is no tuple where d =3.
+select * from bm_table_foo where d = 3 and (d = 1 or d = 2);
+
+-- If a segment contains tuples with d in (1, 2), then it will create the whole StreamNode tree, 
+-- otherwise segments will eliminate nodes.
+select * from bm_table_foo where d = 2 and (d = 1 or d = 2);
+