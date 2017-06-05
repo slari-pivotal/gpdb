feeab48e889fc4ea21539cb5f6c516fe8e12d4e3
Venkatesh Raghavan <vraghavan@pivotal.io>
2017-06-05 11:38:54 -0700

    Update GPORCA fallback messages when optimizer_enable_ctas is set to off

--- src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
+++ src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
@@ -413,7 +413,7 @@ CTranslatorQueryToDXL::CheckSupportedCmdType
 	{		
 		if (!optimizer_enable_ctas && NULL != pquery->intoClause)
 		{
-			GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature, GPOS_WSZ_LIT("CTAS"));
+			GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature, GPOS_WSZ_LIT("CTAS. Set optimizer_enable_ctas to on to enable CTAS with PQO"));
 		}
 		
 		// supported: regular select or CTAS when it is enabled
