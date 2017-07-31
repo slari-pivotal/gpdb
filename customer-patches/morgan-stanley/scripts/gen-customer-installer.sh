bf7aef66c14e3f76efa96b30ad651f2408e8d732
Mike Roth <mroth@pivotal.io>
2017-07-31 13:20:49 -0400

    fixed file references

--- customer-patches/morgan-stanley/scripts/gen-customer-installer.sh
+++ customer-patches/morgan-stanley/scripts/gen-customer-installer.sh
@@ -68,10 +68,10 @@ CONN_INSTALLER_FILE=${CONN_INSTALLER_FILE:=${BASE_DIR}/installer_rhel5_gpdb_conn
 CLIENTS_INSTALLER_FILE=${CLIENTS_INSTALLER_FILE:=${BASE_DIR}/installer_rhel5_gpdb_clients/greenplum-clients-${RELEASE}-build-1-rhel5-x86_64.zip}
 LOADERS_INSTALLER_FILE=${LOADERS_INSTALLER_FILE:=${BASE_DIR}/installer_rhel5_gpdb_loaders/greenplum-loaders-${RELEASE}-build-1-rhel5-x86_64.zip}
 
-PGCRYPTO_GPPKG_FILE=${PGCRYPTO_GPPKG_FILE:=${BASE_DIR}/pgcrypto_rhel5_gppkg/pgcrypto-ossv1.1_pv1.2_gpdb4.3orca-rhel5-x86_64.gppkg}
-PLR_GPPKG_FILE=${PLR_GPPKG_FILE:=${BASE_DIR}/plr_rhel5_gppkg/plr-ossv8.3.0.15_pv2.2.0_gpdb4.3orca-rhel5-x86_64.gppkg}
-PLJAVA_GPPKG_FILE=${PLJAVA_GPPKG_FILE:=${BASE_DIR}/pljava_rhel5_gppkg/pljava-ossv1.5.0_pv1.4.0_gpdb4.3orca-rhel5-x86_64.gppkg}
-MADLIB_GPPKG_FILE=${MADLIB_GPPKG_FILE:=${BASE_DIR}/madlib_rhel5_gppkg/madlib-ossv1.11_pv1.9.8_gpdb4.3orca-rhel5-x86_64.gppkg}
+PGCRYPTO_GPPKG_FILE=${PGCRYPTO_GPPKG_FILE:=${BASE_DIR}/pgcrypto_rhel5_gppkg/pgcrypto-*-rhel5-x86_64.gppkg}
+PLR_GPPKG_FILE=${PLR_GPPKG_FILE:=${BASE_DIR}/plr_rhel5_gppkg/plr-*-rhel5-x86_64.gppkg}
+PLJAVA_GPPKG_FILE=${PLJAVA_GPPKG_FILE:=${BASE_DIR}/pljava_rhel5_gppkg/pljava-*rhel5-x86_64.gppkg}
+MADLIB_GPPKG_FILE=${MADLIB_GPPKG_FILE:=${BASE_DIR}/madlib_rhel5_gppkg/madlib-*rhel5-x86_64.gppkg}
 
 JDBC_DRIVER_FILE=${JDBC_DRIVER_FILE:=${BASE_DIR}/greenplum_jdbc_zip/greenplum_jdbc_5.1.1.zip}
 GPSUPPORT_FILE=${GPSUPPORT_FILE:=${BASE_DIR}/gpsupport_package/gpsupport-1.2.0.0.gz}
