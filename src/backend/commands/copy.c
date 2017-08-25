c65297b9e8bceb4c5bb5e788e231270888c59587
Adam Lee <ali@pivotal.io>
2017-08-25 12:56:21 +0800

    Optimize `COPY TO ON SEGMENT` result processing
    Don't send nonsense '\n' characters just for counting, let segments
    report how many rows are processed instead.
    
    Signed-off-by: Ming LI <mli@apache.org>

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -447,16 +447,6 @@ CopySendEndOfRow(CopyState cstate)
 						(errcode_for_file_access(),
 						 errmsg("could not write to COPY file: %m")));
 
-			/* Send "\n" to QD for "processed" line number counting */
-			if (cstate->on_segment && Gp_role == GP_ROLE_EXECUTE)
-			{
-				if (cstate->ignore_extra_line) /* the csv header */
-					/* ignore the csv header, set the flag */
-					cstate->ignore_extra_line = false;
-				else
-					(void) pq_putmessage('d', "\n", 1);
-			}
-
 			break;
 		case COPY_OLD_FE:
 			/* The FE/BE protocol uses \n as newline for all platforms */
@@ -1244,10 +1234,6 @@ DoCopyInternal(const CopyStmt *stmt, const char *queryString, CopyState cstate)
 			struct stat st;
 			char *filename = cstate->filename;
 
-			/*
-			 * If on_segment, QD receives "\n" for "processed" line number counting, saves
-			 * them to /dev/null to avoid nonsense file
-			 */
 			if (cstate->on_segment && Gp_role == GP_ROLE_DISPATCH)
 				filename = "/dev/null";
 
@@ -2073,7 +2059,6 @@ CopyToDispatch(CopyState cstate)
 		}
 
 		/* add a newline and flush the data */
-		cstate->ignore_extra_line = true; /* CSV header line doesn't count in "processed" line numbers */
 		CopySendEndOfRow(cstate);
 	}
 
@@ -2236,7 +2221,6 @@ CopyTo(CopyState cstate)
 
 				CopyAttributeOutCSV(cstate, colname, false,
 									list_length(cstate->attnumlist) == 1);
-				cstate->ignore_extra_line = true; /* CSV header line doesn't count in "processed" line numbers */
 			}
 
 			CopySendEndOfRow(cstate);
@@ -2392,6 +2376,9 @@ CopyTo(CopyState cstate)
 		/* run the plan --- the dest receiver will send tuples */
 		ExecutorRun(cstate->queryDesc, ForwardScanDirection, 0L);
 	}
+
+	if (Gp_role == GP_ROLE_EXECUTE && cstate->on_segment)
+		SendNumRows(0, cstate->processed);
 
 	MemoryContextDelete(cstate->rowcontext);
 }
