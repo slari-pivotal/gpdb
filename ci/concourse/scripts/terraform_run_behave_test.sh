8d853c6412888ef41bec45df7181f7fa2c606b70
Marbin Tan <mtan@pivotal.io>
2017-09-06 13:56:43 -0700

    Migrate gpcheckcat and gprecoverseg to terraform
    Add pipeline changes and scripts in-order to run terraform within
    GPDB4. There will be many similarities with GPDB5 on how this is setup,
    as that is the reference along with the ccp example done by toolsmiths.

---
+++ ci/concourse/scripts/terraform_run_behave_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BEHAVE_FLAGS=$@
+
+source /usr/local/greenplum-db-devel/greenplum_path.sh
+export PGPORT=5432
+export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
+export PGDATABASE=gptest
+createdb gptest
+cd /home/gpadmin/gpdb_src/gpMgmt
+make -f Makefile.behave behave flags="$BEHAVE_FLAGS"
